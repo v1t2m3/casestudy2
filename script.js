1f6702bdf679bf7e7d7d122b96262835c65e465e
v1t2m3 <nguyenvantamdn@gmail.com>
2021-10-11 11:46:16 +0700

    day 4

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-var owl = $('.owl-carousel');
+var owl = $('.authors_carousel');
 owl.owlCarousel({
-    items:3,
+    items:4,
     loop:true,
-    margin:10,
+    margin:20,
+    smartSpeed: 2000,
     autoplay:true,
     autoplayTimeout:3000,
-    autoplayHoverPause:true
+    autoplayHoverPause:true,
+    
 });
 $('.play').on('click',function(){
     owl.trigger('play.owl.autoplay',[3000])
 })
 $('.stop').on('click',function(){
     owl.trigger('stop.owl.autoplay')
+})
+/////////////////////////////////////////////////////
+var owl2 = $('.acmbooks_carousel');
+owl2.owlCarousel({
+    items:1,
+    loop:true,
+    margin:0,
+    smartSpeed: 2000,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    
+});
+$('.play').on('click',function(){
+    owl2.trigger('play.owl.autoplay',[3000])
+})
+$('.stop').on('click',function(){
+    owl2.trigger('stop.owl.autoplay')
 })
